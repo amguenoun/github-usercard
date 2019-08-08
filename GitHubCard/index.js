3fc016d2970527976ed34fe86dfe64902f929af0
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-08 16:14:34 -0500

    finished followersArray and adjusted all axios calls to work with createCards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,13 +3,13 @@
            https://api.github.com/users/<your name>
 */
 
-// axios.get("https://api.github.com/users/amguenoun")
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   })
+axios.get("https://api.github.com/users/amguenoun")
+  .then(response => {
+    createCard(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -17,22 +17,6 @@
 
    Skip to Step 3.
 */
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -54,13 +38,7 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's:
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+const cards = document.querySelector('.cards');
 
 function createCard(object) {
   //Creating Elements
@@ -82,7 +60,10 @@ function createCard(object) {
   cardName.classList.add('name');
   cardUserName.classList.add('username');
 
+  cardProfile.textContent = 'Profile: '; //Need to be moved up to prevent overwritting anchor tag
+
   //Creating Layout
+  cards.appendChild(card);
   card.appendChild(cardImage);
   card.appendChild(cardInfo);
   cardInfo.appendChild(cardName);
@@ -99,49 +80,34 @@ function createCard(object) {
   cardName.textContent = object.name;
   cardUserName.textContent = object.login;
   cardLocation.textContent = `Location: ${object.location}`;
-  cardProfile.textContent = 'Profile: ';
   cardLink.href = object.html_url;
   cardLink.textContent = object.html_url;
   cardFollowers.textContent = `Followers: ${object.followers}`;
   cardFollowing.textContent = `Following: ${object.following}`;
   cardBio.textContent = `Bio: ${object.bio}`;
-
-  return card;
 }
 
-const data = {
-  "login": "amguenoun",
-  "id": 45905506,
-  "node_id": "MDQ6VXNlcjQ1OTA1NTA2",
-  "avatar_url": "https://avatars0.githubusercontent.com/u/45905506?v=4",
-  "gravatar_id": "",
-  "url": "https://api.github.com/users/amguenoun",
-  "html_url": "https://github.com/amguenoun",
-  "followers_url": "https://api.github.com/users/amguenoun/followers",
-  "following_url": "https://api.github.com/users/amguenoun/following{/other_user}",
-  "gists_url": "https://api.github.com/users/amguenoun/gists{/gist_id}",
-  "starred_url": "https://api.github.com/users/amguenoun/starred{/owner}{/repo}",
-  "subscriptions_url": "https://api.github.com/users/amguenoun/subscriptions",
-  "organizations_url": "https://api.github.com/users/amguenoun/orgs",
-  "repos_url": "https://api.github.com/users/amguenoun/repos",
-  "events_url": "https://api.github.com/users/amguenoun/events{/privacy}",
-  "received_events_url": "https://api.github.com/users/amguenoun/received_events",
-  "type": "User",
-  "site_admin": false,
-  "name": "Ahmad Guenoun",
-  "company": null,
-  "blog": "",
-  "location": null,
-  "email": null,
-  "hireable": null,
-  "bio": null,
-  "public_repos": 18,
-  "public_gists": 0,
-  "followers": 16,
-  "following": 5,
-  "created_at": "2018-12-15T22:18:19Z",
-  "updated_at": "2019-08-08T19:12:35Z"
-}
+/* Step 4: Pass the data received from Github into your function, 
+           create a new component and add it to the DOM as a child of .cards
+*/
 
-const cards = document.querySelector('.cards');
-cards.appendChild(createCard(data));
+/* Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
+*/
+
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+followersArray.forEach(item => {
+  axios.get("https://api.github.com/users/" + item)
+    .then(response => {
+      createCard(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
