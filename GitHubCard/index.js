7ba196898ec66a74670136434077faee2ba578c5
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-08 15:32:48 -0500

    created elements for createCard component function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,13 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get("https://api.github.com/users/amguenoun")
-  .then(response => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.log(err);
-  })
+
+// axios.get("https://api.github.com/users/amguenoun")
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -59,3 +61,20 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function createCard(object) {
+  //Creating Elements
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardName = document.createElement('h3')
+  const cardUserName = document.createElement('p');
+  const cardLocation = document.createElement('p');
+  const cardProfile = document.createElement('p');
+  const cardLink = document.createElement('a');
+  const cardFollowers = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
+
+
+}
